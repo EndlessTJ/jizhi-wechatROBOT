23d4208830a01339691a2e746eb98a2e2a052f7d
jinta <1294546949@qq.com>
2018-01-24 17:09:18 +0800

    refactor

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const crypto = require('crypto');
+const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var signature, timestamp, nonce, echostr, token, hashArray;
+  token = 'tianjintest';
+  signature = req.query.signature;
+	timestamp = req.query.timestamp;
+	nonce = req.query.nonce;
+	echostr = req.query.echostr;
+	hashArray = [token, timestamp, nonce];
+	hashArray.sort();
+	var hashStr = hashArray.join('');
+	var sha1 = crypto.createHash('sha1');
+	sha1.update(hashStr);
+	var tokenStr = sha1.digest('hex');
+	if (tokenStr === signature) {
+	  res.send(echostr)
+  } else {
+	  res.send('error')
+  }
   res.render('index', { title: 'Express' });
 });
 
