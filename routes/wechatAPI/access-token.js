e1091aa25f45a801b682ae65400be8607aeecfc6
jinta <1294546949@qq.com>
2018-04-19 14:47:29 +0800

    fix:fix path

--- routes/wechatAPI/access-token.js
+++ routes/wechatAPI/access-token.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const get_access = function () {
 	request.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx868510aba91b97ec&secret=08323118a8c497eb9576e468a7c6adee',(error, res, data) => {
-		fs.writeFile('../config/access-token.txt',data,'utf8',function (err) {
+		fs.writeFile(`../../config/access-token.txt`,data,'utf8',function (err) {
 			console.log(err)
 		})
 	});
